bb5831f3c72dc3ccd961eb0f18c2a1cfc1591689
SULTANUM MOBIN <sultanum.mobin@gmail.com>
2025-12-01 00:41:30 +0600

    Configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      animation: {
+        'fade-in': 'fadeIn 0.8s ease-in',
+        'slide-up': 'slideUp 0.8s ease-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        slideUp: {
+          '0%': { 
+            opacity: '0',
+            transform: 'translateY(30px)'
+          },
+          '100%': { 
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        }
+      }
+    },
+  },
+  plugins: [],
+}
