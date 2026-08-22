8ee633ce2e115050c3462e25df8a452bf4b08b12
SULTANUM MOBIN <sultanum.mobin@gmail.com>
2026-08-23 00:55:52 +0600

    prevent FOUC

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,27 @@ import { ToastProvider } from './components/ToastProvider';
 import { ThemeProvider } from './context/ThemeContext';
 import './globals.css';
 
+const themeScript = `
+  (function () {
+    try {
+      const theme = localStorage.getItem('theme');
+
+      if (theme === 'dark') {
+        document.documentElement.classList.add('dark');
+      }
+    } catch (e) {}
+  })();
+`;
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <script dangerouslySetInnerHTML={{ __html: themeScript}}/>
+      </head>
       <body>
         <ThemeProvider>
           <ToastProvider>
